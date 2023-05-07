e8836611e9a5927265b4c831fb5ea49202831b8a
Marina Chemezova <aniramskaya@gmail.com>
2023-05-07 16:21:12 +0500

    Added documentation to Converter

--- bricks/source/Converter.swift
+++ bricks/source/Converter.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// Loads data using another **Query** and then converts the result to a **Target** type
 public class Converter<SourceQuery, Target>: Query where SourceQuery: Query
 {
     public typealias TargetMapper = (SourceQuery.Result) -> Target
@@ -14,11 +15,16 @@ public class Converter<SourceQuery, Target>: Query where SourceQuery: Query
     private let query: SourceQuery
     private let map: TargetMapper
     
+    /// Creates Converter with query and mapper
+    /// - Parameters:
+    ///   - query: An entity implementing **Query** protocol
+    ///   - map: A closure to convert the loaded data from **Query.Result** to a **Target** type
     public init(query: SourceQuery, map: @escaping TargetMapper) {
         self.query = query
         self.map = map
     }
     
+    /// Loads data using **SourceQuery** and then converts the result to a **Target** type using map closure passed in constructor upon creation
     public func load(_ completion: @escaping (Target) -> Void) {
         query.load { [weak self] result in
             guard let self else { return }
