d06aea0273ab1811db62ac929400c9ac7638e78e
Marina Chemezova <aniramskaya@gmail.com>
2023-05-07 17:09:59 +0500

    Added FailableQuery protocol

--- bricks/source/Query.swift
+++ bricks/source/Query.swift
@@ -5,9 +5,17 @@
 //  Created by Марина Чемезова on 07.05.2023.
 //
 
+import Foundation
+
 /// Basic protocol which declares asynchronous load for any data
 public protocol Query {
     associatedtype Result
 
     func load(_ completion: @escaping (Result) -> Void)
+}
+
+/// ``Query`` whose **Result** is ``Swift.Result`` so it may fail and return an error.
+protocol FailableQuery: Query where Result == Swift.Result<Success, Failure> {
+    associatedtype Success
+    associatedtype Failure: Error
 }
