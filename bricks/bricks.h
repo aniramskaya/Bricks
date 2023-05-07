5d25c7a9835585fbcce0d64e02431e29fcd56c28
Marina Chemezova <aniramskaya@gmail.com>
2023-05-07 14:17:49 +0500

    Empty XCode project

---
+++ bricks/bricks.h
@@ -0,0 +1,18 @@
+//
+//  bricks.h
+//  bricks
+//
+//  Created by Марина Чемезова on 07.05.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bricks.
+FOUNDATION_EXPORT double bricksVersionNumber;
+
+//! Project version string for bricks.
+FOUNDATION_EXPORT const unsigned char bricksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bricks/PublicHeader.h>
+
+
